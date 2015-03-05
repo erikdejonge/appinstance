5ac3adf764bc21c1e5e59f147bf11a342cf5eca3
Erik de Jonge <erik@a8.nl>
2015-03-05 14:50:50 +0100

    unittester Thursday 05 March 2015 (week:9 day:64), 14:50:49

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ license: GNU-GPL2
 """
 from setuptools import setup
 setup(name='appinstance',
-      version='0.51',
+      version='0.5.2',
       description='Check if an app with the same name is running, supports parameters.',
       url='https://github.com/erikdejonge/appinstance',
       author='Erik de Jonge',
