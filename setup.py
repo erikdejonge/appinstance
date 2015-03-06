d8f0bcf79d06b5ea2eddb18c1fa90c4917e512bd
Erik de Jonge <erik@a8.nl>
2015-03-06 12:18:54 +0100

    -

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ license: GNU-GPL2
 """
 from setuptools import setup
 setup(name='appinstance',
-      version='5',
+      version='6',
       description='Check if an app with the same name is running, supports parameters.',
       url='https://github.com/erikdejonge/appinstance',
       author='Erik de Jonge',
       author_email='erik@a8.nl',
       license='GPL',
       packages=['appinstance'],
-      zip_safe=True)
+      zip_safe=True,
+      install_requires=['psutil', 'hashlib'])
